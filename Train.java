34a66db977fb4a5be380b9a3bcb2a55b4f0706e6
JasLee07 <jrlee@smith.edu>
2026-02-24 21:52:57 -0500

    Stuck heavily on the train

--- Train.java
+++ Train.java
@@ -1,3 +1,19 @@
-public class Train {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Train implements TrainRequirements{
+    private Engine engine;
+    private List<Car> CarList;
+
+public Train(FuelType fuelType, double maxFuellevel, double currentFuellevel, int nCars, int passengerCapacity){
+    this.engine = new Engine(fuelType, maxFuellevel, currentFuellevel);
+    this.CarList = new ArrayList<>(nCars);
+        for (int i = 0; i < nCars; i++){
+            this.CarList.add(new Car (passengersOnBoard, passengerCapacity));
+        }
+}
+
+public Engine getEngine(){
+    return engine;
+}
 }
